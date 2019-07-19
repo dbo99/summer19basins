58e17384f70f58355b4dc875519b9f24fbe6f18b
dbo99 <oconnor.david.b@gmail.com>
2019-07-19 06:43:06 -0700

    Add files via upload

--- areaelev_thursnght.r
+++ areaelev_thursnght.r
@@ -2,8 +2,12 @@
 library(tidyverse)
 library(plotly)
 
+# thinking clearly ## workflow :
 
-{
+# clip every zone, save each dem,
+
+
+#{
 rm(list = ls())
 set.seed(1)
 gridft_1 <- rnorm(200000, 2750, 30) %>% as.integer()
@@ -24,7 +28,7 @@ tccc1huf <- tccc1huf %>% transmute(gridft_2 = as.character(Var1),
                                    count = Freq)
 as_tibble(tccc1huf)
 tccc1huf <- rep(tccc1huf$gridft_2, tccc1huf$count)
-}
+#}
 
 ######### base R hist() and table ####
 f <- ecdf(tccc1hlf)
@@ -78,5 +82,42 @@ df <- data.frame(x = c(rnorm(100, 0, 3), rnorm(100, 0, 10)),
                  g = gl(2, 100))
 ggplot(df, aes(x, colour = g)) + stat_ecdf()
 
-ggplot(tccc1, aes(gridft)) + stat_ecdf() + coord_flip() +facet_wrap(~zone, nrow = 1, scales = "free")
+p <- ggplot(tccc1, aes(gridft)) + stat_ecdf() + coord_flip() +facet_wrap(~zone, nrow = 1, scales = "free")
+p
+ggplotly(p)
+
+
+# youtube ---------------------https://www.youtube.com/watch?v=zRMikMaQE3I
+library(raster)
+ras <- raster("bigraster.tif")
+raszone <- raster("rasterzones.tif")
+ras_zstats <- zonal(ras, raszone, fun = 'mean', digits = 3, na.rm = T)
+
+
+
+
+If I have an integer raster, say of ground elevation data for a country, 
+and one polygon shapefile, say of 300 river basins in that country with a 
+unique name for each, how would I most easily get output like this:
+  
+basin, gridcellelevation
+a, 320
+a, 321
+a, 321
+b, 17,
+b, 18,
+b, 19,
+
+The most burdensome way seems that I could turn the single shapefile into 300 shapefiles, 
+clip the raster 300 times into uniqueID rasters, read them in, generate
+individual tables for each basin, then finally rbind() all together. The ideal
+seems to be able to create that same table with just the one raster and one shapefile
+I'm not looking for any statistics or counts, basically just the raw data listing of the grid
+cell elevations that would have been part of a clip.  I don't have any data at the moment,
+so I'd just be grateful for any tips or libraries or functions that could achieve this, so
+I can look into them before I have access to the data soon (in a day or so).
+
+
+
+
 
