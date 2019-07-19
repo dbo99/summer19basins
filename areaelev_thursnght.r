8d29190d24d3820ecaa67a49f3120d5f40359ead
dbo99 <oconnor.david.b@gmail.com>
2019-07-18 21:40:27 -0700

    Add files via upload

--- areaelev_thursnght.r
+++ areaelev_thursnght.r
@@ -15,13 +15,41 @@ as_tibble(tccc1hlf)
 tccc1hlf <- rep(tccc1hlf$gridft, tccc1hlf$count)
 }
 
-
+######### base R hist() and table ####
 f <- ecdf(tccc1hlf)
 p <- plot(f, main="Default Plot")
 p
 
-
-
 h <- hist(tccc1hlf, breaks = 20)
 cum.prob <- data.frame(value=h$mids, prob=cumsum(h$counts)/sum(h$counts))
 cum.prob
+
+############# ggplot ##########
+
+f <- ecdf(tccc1hlf)
+plot(f, main="Default Plot")
+
+
+expand <- function(x, f=1.05, ...) {# Expand a data range
+  r <- range(x, ...)
+  (r - mean(r)) * f + mean(r) 
+}
+X <- with(environment(f), {
+  x.range <- expand(x)
+  data.frame(x=c(x.range[1], x), 
+             xend=c(x, x.range[2]),
+             y=c(0, y),
+             yend=c(0, y))
+})
+
+
+ 
+p <- ggplot(X, aes(x=x, y=y, xend=xend, yend=yend)) + 
+  geom_hline(yintercept=0:1, linetype=2, size=1.0, color="Gray") +
+  geom_segment(color="#b0b0b0", size=0.8) +
+  geom_point(data=X[-1,]) +
+  xlab("Elevation") + ylab("Proportion of Total") + 
+  ggtitle("Hypsometric Curve") + coord_flip()
+
+p
+
