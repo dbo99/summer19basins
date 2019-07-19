a42eee036a1f4d3edfe5a016d7fd5a8a47bb34fd
dbo99 <oconnor.david.b@gmail.com>
2019-07-18 22:15:06 -0700

    Add files via upload

--- areaelev_thursnght.r
+++ areaelev_thursnght.r
@@ -5,14 +5,25 @@ library(plotly)
 
 {
 rm(list = ls())
-gridft <- rnorm(200000, 2750, 30) %>% as.integer()
-df <- data.frame(gridft) #%>% mutate(zone = "a") 
+set.seed(1)
+gridft_1 <- rnorm(200000, 2750, 30) %>% as.integer()
+df <- data.frame(gridft_1) #%>% mutate(zone = "a") 
 tccc1hlf <- as.data.frame(table(unlist(df))) 
-tccc1hlf <- tccc1hlf %>% transmute(gridft = as.character(Var1),
-                                   gridft = as.integer(gridft),
+tccc1hlf <- tccc1hlf %>% transmute(gridft_1 = as.character(Var1),
+                                   gridft_1 = as.integer(gridft_1),
                                    count = Freq)
 as_tibble(tccc1hlf)
-tccc1hlf <- rep(tccc1hlf$gridft, tccc1hlf$count)
+tccc1hlf <- rep(tccc1hlf$gridft_1, tccc1hlf$count)
+
+set.seed(2)
+gridft_2 <- rnorm(200000, 6750, 60) %>% as.integer()
+df <- data.frame(gridft_2) #%>% mutate(zone = "a") 
+tccc1huf <- as.data.frame(table(unlist(df))) 
+tccc1huf <- tccc1huf %>% transmute(gridft_2 = as.character(Var1),
+                                   gridft_2 = as.integer(gridft_2),
+                                   count = Freq)
+as_tibble(tccc1huf)
+tccc1huf <- rep(tccc1huf$gridft_2, tccc1huf$count)
 }
 
 ######### base R hist() and table ####
@@ -25,6 +36,10 @@ cum.prob <- data.frame(value=h$mids, prob=cumsum(h$counts)/sum(h$counts))
 cum.prob
 
 ############# ggplot ##########
+
+tccc1hlf <- data.frame(tccc1hlf) %>% transmute(zone = "tccc1hlf", gridft = tccc1hlf)
+tccc1huf <- data.frame(tccc1huf) %>% transmute(zone = "tccc1huf", gridft = tccc1huf)
+tccc1 <- rbind(tccc1hlf, tccc1huf)
 
 f <- ecdf(tccc1hlf)
 plot(f, main="Default Plot")
@@ -52,4 +67,16 @@ p <- ggplot(X, aes(x=x, y=y, xend=xend, yend=yend)) +
   ggtitle("Hypsometric Curve") + coord_flip()
 
 p
+
+
+
+#### 
+
+# https://stackoverflow.com/questions/3544002/easier-way-to-plot-the-cumulative-frequency-distribution-in-ggplot
+
+df <- data.frame(x = c(rnorm(100, 0, 3), rnorm(100, 0, 10)),
+                 g = gl(2, 100))
+ggplot(df, aes(x, colour = g)) + stat_ecdf()
+
+ggplot(tccc1, aes(gridft)) + stat_ecdf() + coord_flip() +facet_wrap(~zone, nrow = 1, scales = "free")
 
